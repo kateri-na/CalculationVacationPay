576011b9f959a92840349af319f2550b654cfb65
katerina <katerina_panina1@mail.ru>
2024-03-31 18:29:59 +0300

    Initial commit: create spring boot project

---
+++ src/main/java/ru/panina/neoflexTask/CalculationVacationPay/CalculationVacationPayApplication.java
@@ -0,0 +1,13 @@
+package ru.panina.neoflexTask.CalculationVacationPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationVacationPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationVacationPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/panina/neoflexTask/CalculationVacationPay/CalculationVacationPayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.panina.neoflexTask.CalculationVacationPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationVacationPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
